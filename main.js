2fc457eb41d0499a1ddbc5ea9f887dadb8b00868
RPathare <rutupathare123@gmail.com>
2024-01-15 16:54:00 +0530

    add info

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var typed= new typed(".text",{
-    Strings:["FrontEnd develpoer","Yoytuber","Web Developer"],
+var typed = new Typed(".text",{
+    strings:['FrontEnd Developer','Web Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
-})
+});
+
